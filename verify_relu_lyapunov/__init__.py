32a8449b38bb52284bc9eac4de41d92f4053bfe9
byarbrough <6315292+byarbrough@users.noreply.github.com>
2026-03-10 09:57:11 -0400

    refactor to module

---
+++ verify_relu_lyapunov/__init__.py
@@ -0,0 +1,9 @@
+"""Hyperplane arrangement verification for ReLU neural Lyapunov functions."""
+
+from .lyapunov import (
+    LyapunovProblem,
+    lyapunov_loss_function,
+    train_lyapunov_2d,
+    fine_tune_on_counterexamples,
+)
+from .hyperplane import full_method
